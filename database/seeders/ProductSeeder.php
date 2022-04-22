5a94480bae33692eb8769d2289d87bbad089bd1d
sombat_buasing <sombat.buasing@gmail.com>
2022-04-22 16:54:02 +0700

    Seeder error

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -28,7 +28,7 @@ class ProductSeeder extends Seeder
                 'description' => 'Similique molestias exercitationem officia aut. Itaque doloribus et rerum voluptate iure. Unde veniam magni dignissimos expedita eius',
                 'price' => 18500.00,
                 'image' => 'https://via.placeholder.com/800x600.png/008876?text=samsung',
-                'user_id' => 1,
+                'user_id' => 5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
